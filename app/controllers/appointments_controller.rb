49c28016391adf9b6f014cbbaa4602c90807c62f
Hrishi Mittal <hrishimittal@gmail.com>
2017-01-17 17:23:14 +0000

    Add create method to appointments controller

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -3,4 +3,14 @@ class AppointmentsController < ApplicationController
     @appointments = Appointment.order('appt_time ASC')
     @appointment = Appointment.new
   end
+
+  def create
+    @appointment = Appointment.create(appointment_params)
+    redirect_to :root
+  end
+
+  private
+  def appointment_params
+    params.require(:appointment).permit(:title, :appt_time)
+  end
 end
