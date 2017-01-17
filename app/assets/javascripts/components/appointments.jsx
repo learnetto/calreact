6bdd01f74e2698fc8fff2f08fc8cd29319a3c481
Hrishi Mittal <hrishimittal@gmail.com>
2017-01-17 17:34:42 +0000

    Remove Haml form and list of appointments. Replace with React component for list of appointments.

--- app/assets/javascripts/components/appointments.jsx
+++ app/assets/javascripts/components/appointments.jsx
@@ -1,7 +1,16 @@
 var Appointments = React.createClass({
   render: function() {
     return (
-      <h1>CalReact</h1>
+      <div>
+        {this.props.appointments.map(function(appointment){
+          return (
+            <div>
+              <h3>{appointment.title}</h3>
+              <p>{appointment.appt_time}</p>
+            </div>
+          )
+        })}
+      </div>
     )
   }
 });
