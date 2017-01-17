c3ffe110dcb09029abbfd3ffa8d194051f683980
Hrishi Mittal <hrishimittal@gmail.com>
2017-01-17 18:03:13 +0000

    Handle user input into form text fields and set state

--- app/assets/javascripts/components/appointment_form.jsx
+++ app/assets/javascripts/components/appointment_form.jsx
@@ -1,11 +1,24 @@
 var AppointmentForm = React.createClass({
+  handleChange: function(e) {
+    var name = e.target.name;
+    obj = {};
+    obj[name] = e.target.value;
+    console.log(obj);
+    console.log(this.props.onUserInput);
+    this.props.onUserInput(obj);
+  },
+
   render: function() {
     return (
       <div>
         <h2>Make a new appointment</h2>
         <form>
-          <input name='title' placeholder='Appointment Title' value={this.props.input_title} />
-          <input name='appt_time' placeholder='Date and Time' value={this.props.input_appt_time} />
+          <input name='title' placeholder='Appointment Title'
+            value={this.props.input_title}
+            onChange={this.handleChange} />
+          <input name='appt_time' placeholder='Date and Time'
+            value={this.props.input_appt_time}
+            onChange={this.handleChange}  />
           <input type='submit' value='Make Appointment' />
         </form>        
       </div>
--- app/assets/javascripts/components/appointments.jsx
+++ app/assets/javascripts/components/appointments.jsx
@@ -2,15 +2,21 @@ var Appointments = React.createClass({
   getInitialState: function() {
     return {
       appointments: this.props.appointments,
-      input_title: 'Team standup meeting',
-      input_appt_time: '25 January 2016 9am'
+      title: 'Team standup meeting',
+      appt_time: '25 January 2016 9am'
     }
+  },
+
+  handleUserInput: function(obj) {
+    this.setState(obj);
   },
 
   render: function() {
     return (
       <div>
-        <AppointmentForm input_title={this.state.input_title} input_appt_time={this.state.input_appt_time} />
+        <AppointmentForm input_title={this.state.title}
+          input_appt_time={this.state.appt_time}
+          onUserInput={this.handleUserInput} />
         <AppointmentsList appointments={this.state.appointments} />
       </div>
     )
