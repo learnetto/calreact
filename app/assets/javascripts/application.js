48df67df0e1bb5e451b9007e0b6d3d56728cbdd0
Hrishi Mittal <hrishimittal@gmail.com>
2017-02-07 15:27:10 +0000

    Remove react-rails and install react_on_rails gem

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,9 +13,4 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require react
-//= require react_ujs
-//= require moment
-//= require react-datetime.min
-//= require components
 //= require_tree .
--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,5 @@ module Calreact
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.react.addons = true
   end
 end
